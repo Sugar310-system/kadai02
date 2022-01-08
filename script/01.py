43d8c0951f03bed37066421004eed26d4aa0d301
Sugar310-system <76143173+Sugar310-system@users.noreply.github.com>
2022-01-08 11:43:11 +0900

    Update 01.py

--- script/01.py
+++ script/01.py
@@ -1,10 +1,10 @@
 
 
 import rospy
-from std_msgs.msg import Int64
+from std_msgs.msg import Int32
 rospy.init_node('count')
-pub = rospy.Publisher('count_up' , Int64, queue_size=2)
-rate = rospy.Rate(30)
+pub = rospy.Publisher('count_up' , Int32, queue_size=2)
+rate = rospy.Rate(5)
 n = 0
 while not rospy.is_shutdown():
   n += 1
