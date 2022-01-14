226409dfe3c9227b745033a33ea236fc52665b59
Sugar310-system <76143173+Sugar310-system@users.noreply.github.com>
2022-01-14 12:12:29 +0900

    Create sphere.py
    半径dataの球の体積を表示する

---
+++ scripts/sphere.py
@@ -0,0 +1,11 @@
+import rospy
+import math
+
+from std_msgs.msg import Int32
+def cb(message):
+  global n
+  n = message.data**3*math.pi*4/3
+if __name__=='__main__':   
+rospy.init_node('twice')
+sub = rospy.Subscriber('count_up' , Int32, cb)
+rospy.spin()
