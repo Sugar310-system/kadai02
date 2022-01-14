3e2e5caf079d87f439cda00e51ba518d19cee071
Sugar310-system <76143173+Sugar310-system@users.noreply.github.com>
2022-01-14 14:25:36 +0900

    Update and rename twice.py to cube.py
    一辺が(生成された数値)の立方体の体積を表示します

--- scripts/cube.py
+++ scripts/cube.py
@@ -2,8 +2,8 @@ import rospy
 from std_msgs.msg import Int32
 def cb(message):
   global n
-  n = message.data*2
+  n = message.data**3
 if __name__=='__main__':   
-rospy.init_node('twice')
+rospy.init_node('cube')
 sub = rospy.Subscriber('count_up' , Int32, cb)
 rospy.spin()
