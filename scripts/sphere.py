2a3aa78b0cff4a95a5b7bdd628739f4c9cbef6cf
Sugar310-system <76143173+Sugar310-system@users.noreply.github.com>
2022-01-14 12:14:26 +0900

    Update sphere.py

--- scripts/sphere.py
+++ scripts/sphere.py
@@ -6,6 +6,6 @@ def cb(message):
   global n
   n = message.data**3*math.pi*4/3
 if __name__=='__main__':   
-rospy.init_node('twice')
+rospy.init_node('sphere')
 sub = rospy.Subscriber('count_up' , Int32, cb)
 rospy.spin()
