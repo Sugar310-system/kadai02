0bbe12bfb82b73f1e1f78b5a1a1793de1877ba89
Sugar310-system <76143173+Sugar310-system@users.noreply.github.com>
2022-01-23 11:47:00 +0900

    Update cube.py

--- scripts/cube.py
+++ scripts/cube.py
@@ -5,6 +5,6 @@ def cb(message):
   global n
   n = message.data**3
 if __name__=='__main__':   
-rospy.init_node('cube')
-sub = rospy.Subscriber('count_up' , Int32, cb)
-rospy.spin()
+ rospy.init_node('cube')
+ sub = rospy.Subscriber('count_up' , Int32, cb)
+ rospy.spin()
