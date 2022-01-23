0dcb43b531489f78c5a787753ffec7e482a0b111
Sugar310-system <76143173+Sugar310-system@users.noreply.github.com>
2022-01-23 11:50:38 +0900

    Update pi.py

--- scripts/pi.py
+++ scripts/pi.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 import rospy
-import math
-
 from std_msgs.msg import Int32
+
 def cb(message):
-  global n
-  n = message.data**2*math.pi
-if __name__=='__main__':   
-rospy.init_node('')
-sub = rospy.Subscriber('count_up' , Int32, cb)
-rospy.spin()
+    rospy.loginfo(message.data**2*math.pi)
+
+if __name__ == '__main__':
+    rospy.init_node('twice')
+    sub = rospy.Subscriber('count_up', Int32, cb)
+    rospy.spin()
